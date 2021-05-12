a824caeeb218a200e4d841b482ef83bffc92bfaa
Hyunji Kim <69276681+creativehkim@users.noreply.github.com>
2021-05-12 03:15:11 -0400

    AddTask form toggle added to the 'add'button

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AddTask from './components/AddTask';
 
 
 function App() {
+  const [showAddTask, setShowAddTask]= useState(false)
   const [ tasks, setTasks ] = useState([
     {
       id: 1,
@@ -49,8 +50,9 @@ function App() {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask}/>
+      <Header onAdd={() => setShowAddTask(!showAddTask)}/>
+      {showAddTask && <AddTask onAdd={addTask}/>}
+      
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 
       'No Tasks To Show'}
     </div>
